5c50c03ee2fb2d1ecebef7451b4660bdec83f872
Paul Ventura <j.paul.ventura@gmail.com>
2024-05-05 12:39:37 -0500

    Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,53 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../provider/authProvider";
+import { useState } from "react";
 
 const Login = () => {
     const { setToken } = useAuth();
+    
+    //const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     const navigate = useNavigate();
-  
+
     const handleLogin = () => {
-      setToken("this is a test token");
-      navigate("/", { replace: true });
+        
+            setToken("this is a test token");
+            navigate("/", { replace: true });
+
+
     };
-  
-    setTimeout(() => {
-      handleLogin();
-    }, 3 * 1000);
-  
-    return <>Login Page</>;
+
+    if(password=='123'){
+        setTimeout(() => {
+        handleLogin();
+        }, 1 * 1000);
+    }
+    return (
+        <>
+            <main style={{ padding: '50px' }}>
+                <h1>Login </h1>
+                <br />
+
+                <form>
+                <input type="text" placeholder="Username" />
+                <br />
+                <br />
+
+                <input
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <br />
+
+                <button type="submit">Login</button>
+                </form>
+            </main>
+        </>
+    );
   };
   
   export default Login;
